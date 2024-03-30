d9da21e2abfb8f01e93307ec80a4958af35c51ee
khushbu kumari <khushbulike7@gmail.com>
2024-03-30 14:19:58 +0530

    files-updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg'
 // import './App.css'
 import Navbar from "./components/navbar/Navbar.jsx";
 import Hero from "./components/hero-sec/Hero.jsx"
